89e398fdec46dd0fe3eec2f285e9e75aa0890db2
saurabhomer258 <saurabhomer258@gmail.com>
2018-12-06 18:54:04 +0530

    UI update

---
+++ app/src/main/java/com/example/saurabhomer/cityprobe/CardMenu.java
@@ -0,0 +1,13 @@
+package com.example.saurabhomer.cityprobe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_menu);
+    }
+}
---
+++ app/src/main/java/com/example/saurabhomer/cityprobe/Graphset.java
@@ -0,0 +1,13 @@
+package com.example.saurabhomer.cityprobe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Graphset extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphset);
+    }
+}
